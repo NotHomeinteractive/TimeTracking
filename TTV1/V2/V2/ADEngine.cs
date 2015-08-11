e837d0cc3c37bd0df8123821ba1f1a42ca5dfa0e
cibergod <cibergod1@gmail.com>
2015-08-11 13:51:00 +0300

    Эксперемент с файлом конфигурации
    Записываем или читаем параметры файла конфига

---
+++ TTV1/V2/V2/ADEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//Отдельный класс для работы с AD 
+namespace V2
+{
+    public class ADEngine
+    {
+
+
+    }
+}
---
+++ TTV1/V2/V2/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace V2
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label testL = new Label()
+            {
+                Text = "Test Param"
+            };
+
+            TextBox MyTextBox = new TextBox()
+            {
+                Width = 500
+            };
+
+            Button OK = new Button()
+            {
+                Text = "OK"
+            };
+
+        }
+    }
+}
