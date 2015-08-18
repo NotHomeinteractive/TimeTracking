8337fe62e2ece0e411d6c6de59f833bd9b0af18b
cibergod <cibergod1@gmail.com>
2015-08-18 17:30:18 +0300

    Получение имени пользователя и пароля

---
+++ TTV1/SessionLess/SessionLess/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+using System.IO;
+
+namespace SessionLess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while(true)
+            //{
+
+              //  System.Diagnostics.Eventing.Reader.EventLogSession.GlobalSession.
+
+                string Username = Environment.UserName;
+
+                string Hostname = Environment.MachineName;
+
+            //    Console.Clear();
+            //    Console.WriteLine(" user " + Username + " host " + Hostname);
+                
+            //    //File.WriteAllText("log.txt",Username);
+
+                Thread.Sleep(10000);
+            //}
+        }
+    }
+}
